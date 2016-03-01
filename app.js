83d8c4c658a3c49de0f7586ce6bb0329d2773737
Ham Chao <mspire279@gmail.com>
2016-03-01 20:23:11 +0700

    Can chat -username -message
    Limit messages log 100 messages.

--- app.js
+++ app.js
@@ -3,9 +3,35 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+var messages = [];
+
+var storeMsg = function(author, msg){
+	messages.push({
+		author: author,
+		msg: msg
+	});
+	if(messages.length > 100){
+		messages.shift();
+	}
+}
+
 io.on('connection', function(client){
 	console.log("Client connected.");
+	messages.forEach(function(message){
+		client.emit('messages', message.author + ": " + message.msg);
+	});
+	client.on('messages', function(message){
+		console.log(message);
+		var username = client.username;
+		client.emit('messages', username + ": " + message);
+		client.broadcast.emit('messages', username + ": " + message);
+		storeMsg(username, message);
+	});
+	client.on('join', function(username){
+		client.username = username;
+	});
 });
+
 
 app.get('/', function(request, response){
 	response.sendFile(__dirname + '/index.html');
